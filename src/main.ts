257ad3ca6d67caa5ee05faf318c37d650b928163
Aofparinya <75320296+Aofparinya@users.noreply.github.com>
2023-04-27 01:00:14 +0700

    add stucture for crete microservice in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  /* for create micro service 
+  const logger = new Logger();
+  const PORT = 3001;
+
+  const app = await NestFactory.createMicroservice(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: { port: PORT },
+    },
+  )
+  */
+
   const config = new DocumentBuilder()
-  .setTitle('E-commerce')
-  .setDescription('Test Back-end Neversitup')
-  .setVersion('1.0')
-  .build();
+    .setTitle('E-commerce')
+    .setDescription('Test Back-end Neversitup')
+    .setVersion('1.0')
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
