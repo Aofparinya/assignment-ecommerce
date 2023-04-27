f2ea1c14a50cef1faf44ed417add1e88d0fee862
Aofparinya <75320296+Aofparinya@users.noreply.github.com>
2023-04-27 22:43:39 +0700

    remove unused code in product controller and service

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -9,42 +9,22 @@ export class ProductController {
 
     constructor(private productService: ProductService) { }
 
-    /*
-    // view all products
-    @Get('find-all')
-    getProductAll(): ProductDto[] {
-        return this.productService.findAll();
-    }
-
-    // view product by id
-    @Get("findById/:id")
-    getProductById(@Param('id') id: string) {
-        return this.productService.findById(Number(id));
-    }
-    
-    // view product by name 
-    @Get('findByProductname')
-    getProductByName(@Query('productname') productName: string) {
-        return this.productService.findByCondition((product) =>
-        product.name.includes(productName));
-    }
-    */
-
-    @Get("test")
+    // view specific product 
+    @Get("find-product-by-name")
     test(@Query('name') name: string) {
         return this.productService.findByProductName(name);
     }
-
+    
     // view all products
     @Get('find-all-products')
     getAllProducts() {
         return this.productService.findAllProducts();
     }
-
+    
     // create product
     @Post('create-product')
     createProduct(@Body() productDto: ProductDto) {
         return this.productService.createProduct(productDto);
     }
-
+    
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -11,26 +11,6 @@ export class ProductService {
         @InjectRepository(Product) private readonly productRepository: Repository<Product>,
     ) { }
 
-    /*
-    private products: ProductDto[] =
-        [
-            { name: 'Mango', id: 1, price: 250 },
-            { name: 'Apple', id: 2, price: 150 },
-            { name: 'PineApple', id: 3, price: 50 },
-        ];
-
-    findAll(): ProductDto[] {
-        return this.products;
-    }
-
-    findById(id: number) {
-        return this.products.find((p) => p.id === id);
-    }
-
-    findByCondition(predicate: (product: ProductDto) => boolean) {
-        return this.products.filter(p => predicate(p));
-    }
-*/
     async findByProductName(productName: string) {
         let result = await this.productRepository
             .createQueryBuilder("product")
