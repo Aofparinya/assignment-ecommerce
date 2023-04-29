21bab968e440b392c83dcd47edaad0ff58fba24f
Aofparinya <75320296+Aofparinya@users.noreply.github.com>
2023-04-29 11:07:56 +0700

    update create order dto

--- src/dto/create-order-dto.ts
+++ src/dto/create-order-dto.ts
@@ -1,9 +1,10 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Product } from "src/product/entities/product.entity";
+import { User } from "src/users/entities/user.entity";
 
 export class CreateOrderDTO {
     id:number;
-    userId:number;
+    userId:User;
     orderStatus:string
     product:Product;
 }
